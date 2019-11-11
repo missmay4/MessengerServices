8e11b2bbe4bd7db08b3991836d8681d5bbc34779
GeroAlv-Alex <geroal.xander@gmail.com>
2019-11-11 17:10:34 +0100

    Movidas

--- src/controller/userController.php
+++ src/controller/userController.php
@@ -1,9 +1,12 @@
 <?php  
     require_once '../model/userModel.php';
+    require_once '../utils/sessionService.php';
     class userController{
 
         public static function modifyUsers( $user ){
             UserModel::modifyUsers($user);
-            
+            //$user = UserModel::getUserID($_SESSION['user']->getID());
+            //SessionService::manageSession($user);
+            //header('Location : ../views/profile.php');
         }
     }

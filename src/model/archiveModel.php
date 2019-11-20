b4e80e3f4966ddb3f97c74555157cdf6b27d8c07
Mayte <may.eater@gmail.com>
2019-11-20 19:11:44 +0100

    attach file model

--- src/model/archiveModel.php
+++ src/model/archiveModel.php
@@ -15,4 +15,20 @@ class archiveModel{
         }
     }
 
+    public static function AttachArchive ($file){
+        $target_dir = "../views/attachments/";
+        $file_name = $file['name'];
+        $uploadOk = 1;
+
+        if ($uploadOk == 0) {
+            echo "Sorry, your file was not uploaded.";
+        } else {
+            if (move_uploaded_file($file['tmp_name'], $target_dir . $file_name)) {
+                return true;
+            } else {
+                return false;
+            }
+        }
+    }
+
 }

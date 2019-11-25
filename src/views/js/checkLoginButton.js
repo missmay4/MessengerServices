307a22db62d27dbbcfc08640c2ff808c77d1bcea
missmay4 <may.eater@gmail.com>
2019-11-25 01:09:11 +0100

    javascript buttons form

---
+++ src/views/js/checkLoginButton.js
@@ -0,0 +1,36 @@
+function checkData(){
+    var user = document.getElementById('usr').value;
+    var pass = document.getElementById('pswd').value;
+
+    var regexUsr = /\w+/;
+    var regexPass = /\w+\d+/;
+    if (regexUsr.test(user) && regexPass.test(pass)){
+        return false;
+    } else {
+        return true;
+    }
+}
+
+function setUpFormulario(){
+    console.log('set');
+    var user = document.getElementById('usr');
+    var pass = document.getElementById('pswd');
+
+    user.onkeypress = function () {
+        toggleSubmit(checkData());
+    }
+    pass.onkeypress = function () {
+        toggleSubmit(checkData());
+    }
+
+}
+
+function toggleSubmit(state){
+    console.log(state);
+    var boton = document.getElementById('but');
+    boton.disabled = state;
+}
+
+window.onload = function () {
+    setUpFormulario();
+}
---
+++ src/views/js/checkRegisterButton.js
@@ -0,0 +1,47 @@
+function checkData(){
+    var user = document.getElementById('usr').value;
+    var pass = document.getElementById('pswd').value;
+    var mail = document.getElementById('mail').value;
+
+    var regexUsr = /\w+/;
+    var regexPass = /\w+\d+/;
+    var regexMail = /[^@]+@[^\\.]+\..+/;
+
+    console.log(regexMail.test(mail));
+
+    if (regexUsr.test(user) && regexPass.test(pass) && regexMail.test(mail)){
+        return false;
+    } else {
+        return true;
+    }
+
+
+}
+
+function setUpFormulario(){
+    var user = document.getElementById('usr');
+    var pass = document.getElementById('pswd');
+    var mail = document.getElementById('mail');
+
+    user.onkeypress = function () {
+        toggleSubmit(checkData());
+    }
+    pass.onkeypress = function () {
+        toggleSubmit(checkData());
+    }
+
+    mail.onkeypress = function () {
+        toggleSubmit(checkData());
+    }
+
+}
+
+function toggleSubmit(state){
+    //console.log(state);
+    var boton = document.getElementById('but');
+    boton.disabled = state;
+}
+
+window.onload = function () {
+    setUpFormulario();
+}

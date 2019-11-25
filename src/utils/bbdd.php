046ec6ec548468232ff337cf82f30514a01e1787
GeroAlv-Alex <geroal.xander@gmail.com>
2019-11-25 17:36:34 +0100

    Comments utils folder

--- src/utils/bbdd.php
+++ src/utils/bbdd.php
@@ -1,15 +1,17 @@
 <?php
 class BBDD{
 
-    private static $servername = "localhost";
-    private static $username = "MSM";
-    private static $password = "ROOT";
-    private static $squema = "MessengerService";
-
+    private static $servername = "localhost"; //Server Name
+    private static $username = "MSM"; // Database User
+    private static $password = "ROOT"; //Password
+    private static $squema = "MessengerService"; //Database Name
+    /**
+     * Return a Connection Object
+     */
     public static function getConnetion(){
         try {
             $conn = new PDO("mysql:host=". self::$servername .";dbname=". self::$squema , self::$username, self::$password);
-            $conn->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
+            $conn->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION); //Set Error Managment
             return $conn;
         } catch (PDOException $e) {
             echo $e->getMessage();
--- src/utils/sessionService.php
+++ src/utils/sessionService.php
@@ -1,10 +1,22 @@
 <?php
+/**
+ *  Service to centralize session management
+ */
     class SessionService{
-
+        /**
+         * Params a Users Object
+         * to update the session
+         */
         public static function updateSession($usuario){
             $_SESSION['user'] = $usuario;
         } 
-
+        /**
+         * if Params is not null , (Object Users)
+         * set it to session 
+         * return 1 if is logged 
+         * and redirect to login php in other ways
+         * 
+         */
         public static function manageSession( $usuario = null){    
             session_start();
             if(isset($_SESSION['user'])){

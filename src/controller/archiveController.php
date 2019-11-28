f6db748412b344dec05629ff8be9ffa80369899a
missmay4 <may.eater@gmail.com>
2019-11-28 02:10:23 +0100

    comentarios archive controller

--- src/controller/archiveController.php
+++ src/controller/archiveController.php
@@ -6,15 +6,30 @@
  */
 class archiveController{
 
+    /**
+     * @param $archive
+     * @return bool
+     * Save an uploaded archive
+     */
+
     public static function modifyArchive($archive){
         return archiveModel::SaveArchive($archive);
     }
 
+    /**
+     * @param $archive
+     * Attach a file to a message
+     */
     public static function attachArchive($archive){
         date_default_timezone_set('Europe/Madrid');
         return archiveModel::updateAttachments($archive);
     }
 
+    /**
+     * @param $archive
+     * @return bool|string
+     * Save the attachments
+     */
     public static function saveArchive($archive){
         return archiveModel::AttachArchive($archive);
     }
